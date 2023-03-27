ecd64af15372eea86a54232b9aeea4da1c7c6035
PitPot <87196264+PitPot1@users.noreply.github.com>
2023-03-27 08:30:43 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -2,79 +2,200 @@ const entries = {
     "minecraft-dev-category": [
         {
             "title": "Server Plugins",
-            "description": "I have been using the Spigot/Paper API (In Java) for almost 3 years now, I can do anything ranging from simple commands to complex procedural world generation. I have made multiple small servers entirely using custom plugins but my best one so far is called CityMiner, you can take a look by joining CityMiner.minehut.gg (Hosted on minehut), It has procedurally generated cities, custom bosses & entities, special weapons and items and much more... The project is still active to this day.",
-            "image": "./assets/CITYMINER.png"
-        },
-        {
-            "title": "Mods",
-            "description": "I have been making forge mods for on and off for around a year. It is far from being one of my strong points thought.",
-            "image": "./assets/forge-logo.jpg"
+            "description": ["I have been actively making plugins for spigot & paper minecraft servers for over 3 years, here are some of my past projects and commissions."],
+            "image": "./assets/spigot-logo.png",
+            "entries": [
+                {
+                    "title": "Dvine SMP",
+                    "description": [
+                        "Divine SMP is a new SMP server for which I coded a core plugin (basically all the server's functionalities & features).",
+                        "The server has 10 special powers called 'spirits' that each grant unique abilities and buffs,",
+                        "Along with other custom items and game mechanics.",
+                        "If you want to see some of the mentioned features, feel free to watch their trailer at: https://youtu.be/K2BcdYcimcY"
+                    ],
+                    "image": "./assets/spirit-thing.png",
+                    "reviews": [
+                        {
+                            "author": "Derkz",
+                            "stars": 5,
+                            "message": ""
+                        }
+                    ]
+                },
+                {
+                    "title": "CityMiner",
+                    "description": [
+                        "CityMiner is my personal server project that is entirely made from custom plugins that i made.",
+                        "I have been working on it for a couple months now and it is now public.",
+                        "It has features such as:",
+                        "- Procedurally generated structures",
+                        "- Custom bosses & mobs (with special abilities)",
+                        "- A custom item system",
+                        "- Quests and missions",
+                        "And a lot more...",
+                        "CityMiner is the perfect demonstration of what i can do. You can take a closer look by connecting to 'CityMiner.minehut.gg'."
+                    ],
+                    "image": "./assets/CITYMINER.png"
+                },
+                {
+                    "title": "GenCore",
+                    "description": [
+                        "GenCore is a plugin that adds generators to the game (used in the popular gens gamemode) which was made for an upcoming server called DashGenss.",
+                        "It is was made to be as optimized as possible and cause the least amount of lag."
+                    ],
+                    "image": "./assets/dashgenss-gens.png",
+                    "reviews": [
+                        {
+                            "author": "perks",
+                            "stars": 5,
+                            "message": ""
+                        }
+                    ]
+                }
+            ]
         },
         {
             "title": "Skript",
-            "description": "I used Skript in a lot of projects and have a good understanding of how it works, even thought I highly prefer using java over it.",
-            "image": "./assets/skript-logo.jpg"
+            "description": ["I have been using skript for over 3 years now and have a good understanding of how to use it correctly, even thought i would rather use Java plugins instead."],
+            "image": "./assets/skript-logo.jpg",
+            "entries": [
+                {
+                    "title": "Leveling Script",
+                    "description": [
+                        "This script was made for a server called DashGenss and allows players to level up and prestige",
+                        "by doing various actions like mining a specific block, killing a specific mob..."
+                    ],
+                    "image": "./assets/script1.png",
+                    "reviews": [
+                        {
+                            "author": "perks",
+                            "stars": 5,
+                            "message": ""
+                        }
+                    ]
+                }
+            ]
         }
     ],
     "web-dev-category": [
-        {
-            "title": "Web Servers & APIs",
-            "description": "I have been using NodeJS for over a year now and can make high performance, scalable web servers and APIs. This includes websites with advanced functionalities such as authentification, user data saving...",
-            "image": "./assets/nodejs-logo.png"
+        {   
+            "title": "Websites",
+            "description": ["I have been using HTML, CSS, JS and NodeJs for 2+ years and have a good understanding of how to use them correctly and efficiently to make good static or dynamic websites."],
+            "image": "./assets/html-logo.avif",
+            "entries": [
+                {
+                    "title": "Protfolio Website",
+                    "description": [
+                        "This website is a basic showcase of what I can do,",
+                        "You can take a look at its source code on my GitHub."
+                    ],
+                    "image": "./assets/website.png"
+                }
+            ]
         },
         {
-            "title": "Web Pages",
-            "description": "I am good at front end development and have experience with HTML, CSS and JavaScript. Allowing me to make good looking user interfaces.",
-            "image": "./assets/html-logo.avif"
+            "title": "Web Servers",
+            "description": [
+                "I have a lot of experience in making web servers using NodeJs aswel as experience working with linux commands and VPSs.",
+                "Here are some of my previous projects/comissions."
+            ],
+            "image": "./assets/nodejs-logo.png",
+            "entries": [
+                {
+                    "title": "Minecraft Bot",
+                    "description": [
+                        "This commission was requested by a user and included a minecraft client that was controled from a discord bot.",
+                        "This bot would be used to automate certain actions on a minecraft server."
+                    ],
+                    "image": "./assets/botting.png",
+                    "reviews": [
+                        {
+                            "author": "Anonymous",
+                            "stars": 5,
+                            "message": ""
+                        }
+                    ]
+                }
+            ]
         }
     ],
     "minecraft-building-category": [
         {
-            "title": "Skills",
-            "description": "I have been an active minecraft builder for over 4 years, during which i gained experience in most aspects of building. I use WorldEdit as building tool and also know how to use WorldPainter. I would say my strongest area is terraforming and natural themed builds. You can see some of my past builds bellow.",
-            "image": ""
-        },
-        {
-            "title": "Faction Spawn",
-            "description": "This spawn build was made for a small factions server back in 2022, the server is now long gone.",
-            "image": "./assets/5.png"
-        },
-        {
-            "title": "Server Hub",
-            "description": "This hub build was for a network that used to be active around 2022, the build is much bigger but this is the only screenshot that I still have.",
-            "image": "./assets/1.png"
-        },
-        {
-            "title": "Floating Islands",
-            "description": "This small build was made back in 2021 as an experiment to see if I could make good looking floating islands, the result turned out alright.",
-            "image": "./assets/4.png"
+            "title": "Builds",
+            "description": [
+                "I have been making minecraft builds for servers and indivuduals for over 4 years, these are some of my builds.",
+                "Tools i use:",
+                "- WorldEdit",
+                "- WorldPainter"
+            ],
+            "image": "./assets/pickaxe-icon.png",
+            "entries": [
+                {
+                    "title": "Faction Spawn",
+                    "description": ["This spawn build was made for a small factions server back in 2022, the server is now long gone."],
+                    "image": "./assets/5.png",
+                    "reviews": [
+                        {
+                            "author": "Anonymous",
+                            "stars": 5,
+                            "message": ""
+                        }
+                    ]
+                },
+                {
+                    "title": "Server Hub",
+                    "description": ["This hub build was for a network that used to be active around 2022, the build is much bigger but this is the only screenshot that I still have."],
+                    "image": "./assets/1.png",
+                    "reviews": [
+                        {
+                            "author": "Anonymous",
+                            "stars": 5,
+                            "message": ""
+                        }
+                    ]
+                },
+                {
+                    "title": "Floating Islands",
+                    "description": ["This small build was made back in 2021 as an experiment to see if I could make good looking floating islands, the result turned out alright."],
+                    "image": "./assets/4.png"
+                }
+            ]
         }
     ],
     "graphic-design-category": [
         {
-            "title": "Skills",
-            "description": "I have always liked art and graphic design, and I have been practicing my skills for almost 6 years now. Here are some of the designs I made in the past.",
-            "image": ""
-        },
-        {
-            "title": "CityMiner Logo",
-            "description": "CityMiner is a big project of mine that is still active to this day, its logo design is simple but eye-catching.",
-            "image": "./assets/new-CM-logo.png"
-        },
-        {
-            "title": "Frost Network Logo",
-            "description": "Frost Network was a small pation project that sadly never found success, this is the logo I had made for it.",
-            "image": "./assets/logo.png"
-        },
-        {
-            "title": "Koros Network Logo",
-            "description": "Koros Network was a small project I was a part of, I think that the logo turned out great.",
-            "image": "./assets/koros logo.png"
-        },
-        {
-            "title": "FN Logo",
-            "description": "This logo was a comission for a server owner.",
-            "image": "./assets/FN_normal.png"
+            "title": "Logos",
+            "description": ["I have always liked art and graphic design, and I have been practicing my digital art skills for almost 6 years now. Here are some of the designs I made in the past."],
+            "image": "./assets/painting-icon.png",
+            "entries": [
+                {
+                    "title": "CityMiner Logo",
+                    "description": ["CityMiner is a big project of mine that is still active to this day, I wanted its logo design to be simple but eye-catching."],
+                    "image": "./assets/new-CM-logo.png"
+                },
+                {
+                    "title": "Frost Network Logo",
+                    "description": ["Frost Network was a small pation project that sadly never found success, this is the logo I had made for it."],
+                    "image": "./assets/logo.png"
+                },
+                {
+                    "title": "Koros Network Logo",
+                    "description": ["Koros Network was a small project I was a part of, I think that the logo turned out great."],
+                    "image": "./assets/koros logo.png"
+                },
+                {
+                    "title": "FN Logo",
+                    "description": ["This logo was a comission for a server owner."],
+                    "image": "./assets/FN_normal.png",
+                    "reviews": [
+                        {
+                            "author": "Anonymous",
+                            "stars": 5,
+                            "message": ""
+                        }
+                    ]
+                }
+            ]
         }
     ]
 }
@@ -94,24 +215,64 @@ function selectCategory(id) {
     const entriesElement = document.getElementById("entries")
     entriesElement.style.visibility = 'visible'
     entriesElement.innerHTML = ""
-    const categoryEntries = entries[category.id]
-    if (categoryEntries) {
-        categoryEntries.forEach(entry => {
-            entriesElement.appendChild(createEntry(
-                entry.title,
-                entry.image,
-                entry.description
-            ))
-        });
-    }
+    const subCategories = entries[category.id]
+    subCategories.forEach(subCategory => {
+        const subCategoryElement = createSubCategory(
+            subCategory.title,
+            subCategory.image,
+            subCategory.description
+        )
+
+        const categoryEntries = subCategory["entries"]
+        if (categoryEntries) {
+            categoryEntries.forEach(entry => {
+                subCategoryElement.appendChild(createEntry(
+                    entry.title,
+                    entry.image,
+                    entry.description,
+                    entry.reviews
+                ))
+            });
+        }
+
+        entriesElement.appendChild(subCategoryElement)
+    })
 
     previousCategory = category;
+
+    // Scroll down
+    entriesElement.scrollIntoView()
 }
 
 
 
+function createSubCategory(title, img_path, description) {
+    const entry = document.createElement("div")
+    entry.className = "sub-category"
 
-function createEntry(title, img_path, description) {
+    const img_e = document.createElement("img")
+    img_e.className = "image"
+    img_e.src = img_path
+    entry.appendChild(img_e)
+
+    const title_e = document.createElement("p")
+    title_e.className = "title"
+    title_e.textContent = title
+    entry.appendChild(title_e)
+
+    description.forEach(line => {
+        const description_e = document.createElement("p")
+        description_e.className = "description"
+        description_e.textContent = line
+        entry.appendChild(description_e)
+    })
+
+    return entry
+}
+
+
+
+function createEntry(title, img_path, description, reviews) {
     const entry = document.createElement("div")
     entry.className = "entry"
 
@@ -120,15 +281,46 @@ function createEntry(title, img_path, description) {
     title_e.textContent = title
     entry.appendChild(title_e)
 
-    const description_e = document.createElement("p")
-    description_e.className = "description"
-    description_e.textContent = description
-    entry.appendChild(description_e)
+    description.forEach(line => {
+        const description_e = document.createElement("p")
+        description_e.className = "description"
+        description_e.textContent = line
+        entry.appendChild(description_e)
+    })
+    
 
     const img_e = document.createElement("img")
     img_e.className = "image"
     img_e.src = img_path
     entry.appendChild(img_e)
+
+
+    if (reviews) {
+        const reviews_box_e = document.createElement("div")
+        reviews.forEach(review => {
+            const review_box_e = document.createElement("div")
+            review_box_e.className = "review_box"
+
+            const review_author_e = document.createElement("p")
+            review_author_e.className = "review_author"
+            review_author_e.textContent = review.author
+            review_box_e.appendChild(review_author_e)
+
+            const review_stars_e = document.createElement("p")
+            review_stars_e.className = "review_stars"
+            review_stars_e.textContent = "★".repeat(Math.ceil(review.stars))
+            review_box_e.appendChild(review_stars_e)
+
+            const review_message_e = document.createElement("p")
+            review_message_e.className = "review_message"
+            review_message_e.textContent = review.message
+            review_box_e.appendChild(review_message_e)
+
+            reviews_box_e.appendChild(review_box_e)
+        })
+        entry.appendChild(reviews_box_e)
+    }
+    
 
     return entry
 }
