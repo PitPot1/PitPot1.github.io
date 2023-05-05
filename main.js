c8eb4adcb7329c40177d41793b779b77ea8778c7
PitPot <87196264+PitPot1@users.noreply.github.com>
2023-05-06 00:14:15 +0200

    Design improvements + multi image support
    + Changed the background image
    + Changed the title & description positions
    + Added support for multiple images in each sub-category entry

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const entries = {
                         "Along with other custom items and game mechanics.",
                         "If you want to see some of the mentioned features, feel free to watch their trailer at: https://youtu.be/K2BcdYcimcY"
                     ],
-                    "image": "./assets/spirit-thing.png",
+                    "image": ["./assets/spirit-thing.png","./assets/ashessmp-1.png","./assets/ashessmp-2.png"],
                     "reviews": [
                         {
                             "author": "Derkz",
@@ -35,7 +35,7 @@ const entries = {
                         "And a lot more...",
                         "CityMiner is the perfect demonstration of what i can do. You can take a closer look by connecting to 'CityMiner.minehut.gg'."
                     ],
-                    "image": "./assets/CITYMINER.png"
+                    "image": ["./assets/cityminer-1.png", "./assets/cityminer-3.png", "./assets/cityminer-2.png", "./assets/CITYMINER.png"]
                 },
                 {
                     "title": "GenCore",
@@ -43,7 +43,7 @@ const entries = {
                         "GenCore is a plugin that adds generators to the game (used in the popular gens gamemode) which was made for an upcoming server called DashGenss.",
                         "It is was made to be as optimized as possible and cause the least amount of lag."
                     ],
-                    "image": "./assets/dashgenss-gens.png",
+                    "image": ["./assets/dashgenss-gens.png"],
                     "reviews": [
                         {
                             "author": "perks",
@@ -65,7 +65,7 @@ const entries = {
                         "This script was made for a server called DashGenss and allows players to level up and prestige",
                         "by doing various actions like mining a specific block, killing a specific mob..."
                     ],
-                    "image": "./assets/script1.png",
+                    "image": ["./assets/level-skript-1.png", "./assets/script1.png", "./assets/level-skript-2.png"],
                     "reviews": [
                         {
                             "author": "perks",
@@ -89,7 +89,7 @@ const entries = {
                         "This website is a basic showcase of what I can do,",
                         "You can take a look at its source code on my GitHub."
                     ],
-                    "image": "./assets/website.png"
+                    "image": ["./assets/website-1.png"]
                 }
             ]
         },
@@ -107,7 +107,7 @@ const entries = {
                         "This commission was requested by a user and included a minecraft client that was controled from a discord bot.",
                         "This bot would be used to automate certain actions on a minecraft server."
                     ],
-                    "image": "./assets/botting.png",
+                    "image": ["./assets/botting.png"],
                     "reviews": [
                         {
                             "author": "Anonymous",
@@ -133,7 +133,7 @@ const entries = {
                 {
                     "title": "Faction Spawn",
                     "description": ["This spawn build was made for a small factions server back in 2022, the server is now long gone."],
-                    "image": "./assets/5.png",
+                    "image": ["./assets/5.png"],
                     "reviews": [
                         {
                             "author": "Anonymous",
@@ -145,7 +145,7 @@ const entries = {
                 {
                     "title": "Server Hub",
                     "description": ["This hub build was for a network that used to be active around 2022, the build is much bigger but this is the only screenshot that I still have."],
-                    "image": "./assets/1.png",
+                    "image": ["./assets/1.png"],
                     "reviews": [
                         {
                             "author": "Anonymous",
@@ -157,7 +157,7 @@ const entries = {
                 {
                     "title": "Floating Islands",
                     "description": ["This small build was made back in 2021 as an experiment to see if I could make good looking floating islands, the result turned out alright."],
-                    "image": "./assets/4.png"
+                    "image": ["./assets/4.png"]
                 }
             ]
         }
@@ -171,22 +171,22 @@ const entries = {
                 {
                     "title": "CityMiner Logo",
                     "description": ["CityMiner is a big project of mine that is still active to this day, I wanted its logo design to be simple but eye-catching."],
-                    "image": "./assets/new-CM-logo.png"
+                    "image": ["./assets/new-CM-logo.png"]
                 },
                 {
                     "title": "Frost Network Logo",
                     "description": ["Frost Network was a small pation project that sadly never found success, this is the logo I had made for it."],
-                    "image": "./assets/logo.png"
+                    "image": ["./assets/logo.png"]
                 },
                 {
                     "title": "Koros Network Logo",
                     "description": ["Koros Network was a small project I was a part of, I think that the logo turned out great."],
-                    "image": "./assets/koros logo.png"
+                    "image": ["./assets/koros logo.png"]
                 },
                 {
                     "title": "FN Logo",
                     "description": ["This logo was a comission for a server owner."],
-                    "image": "./assets/FN_normal.png",
+                    "image": ["./assets/FN_normal.png"],
                     "reviews": [
                         {
                             "author": "Anonymous",
@@ -272,7 +272,7 @@ function createSubCategory(title, img_path, description) {
 
 
 
-function createEntry(title, img_path, description, reviews) {
+function createEntry(title, img_paths, description, reviews) {
     const entry = document.createElement("div")
     entry.className = "entry"
 
@@ -289,10 +289,29 @@ function createEntry(title, img_path, description, reviews) {
     })
     
 
-    const img_e = document.createElement("img")
-    img_e.className = "image"
-    img_e.src = img_path
-    entry.appendChild(img_e)
+    if (img_paths && img_paths.length > 0) {
+        const preview_box = document.createElement("div")
+        preview_box.className = "preview-box"
+        entry.appendChild(preview_box)
+        
+        const img_path = img_paths[0];
+        const img_e = document.createElement("img")
+        img_e.className = "image"
+        img_e.src = img_path
+
+        for (let i = 0; i < img_paths.length; i++) {
+            const img_preview_path = img_paths[i];
+            const img_preview_e = document.createElement("img")
+            img_preview_e.className = "image-preview"
+            img_preview_e.src = img_preview_path
+            img_preview_e.onclick = () => {
+                img_e.src = img_preview_path
+            }
+            preview_box.appendChild(img_preview_e)
+        }
+
+        entry.appendChild(img_e)
+    }
 
 
     if (reviews) {
